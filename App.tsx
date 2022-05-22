e94c365ecd64ae56dbc2a92b05b1bedcf513d18e
JhonCar <jhon7car@gmail.com>
2022-05-22 17:13:44 -0300

    dependencias

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
+import 'react-native-gesture-handler';
 import React from 'react'
 import { Text, View } from 'react-native';
-import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { Navigator } from './src/navigator/Navigator';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hola RNConponents</Text>
-    </View>
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
   )
 }
 
